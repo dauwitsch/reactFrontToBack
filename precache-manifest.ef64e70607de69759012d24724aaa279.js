8f8ce7d5001bf21f93ca7e0cd2f50c457aa3fb17
Administrator <dst@pmgnet.de>
2019-04-03 09:31:46 +0200

    Updates

---
+++ precache-manifest.ef64e70607de69759012d24724aaa279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7f3d71ea7133a1014ae",
+    "url": "/reactFrontToBack/static/css/main.4017ad16.chunk.css"
+  },
+  {
+    "revision": "d7f3d71ea7133a1014ae",
+    "url": "/reactFrontToBack/static/js/main.d7f4b0d5.chunk.js"
+  },
+  {
+    "revision": "bb53e80acf62ed578abd",
+    "url": "/reactFrontToBack/static/js/runtime~main.50c483cb.js"
+  },
+  {
+    "revision": "7e0baa850ab47f15f2d4",
+    "url": "/reactFrontToBack/static/css/2.1a1dec15.chunk.css"
+  },
+  {
+    "revision": "7e0baa850ab47f15f2d4",
+    "url": "/reactFrontToBack/static/js/2.f1e27ec6.chunk.js"
+  },
+  {
+    "revision": "10535be5f17d35fdeb40220703967e7d",
+    "url": "/reactFrontToBack/index.html"
+  }
+];
